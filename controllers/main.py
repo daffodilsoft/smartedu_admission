d09386bc68c6fd17f77fd6e1a7e4ebce81db9055
imran2437 <alimran689@gmail.com>
2023-06-04 10:18:04 +0600

    new_functions

--- controllers/main.py
+++ controllers/main.py
@@ -3,28 +3,32 @@ from odoo.http import request
 
 
 class applications(http.Controller):
-    @http.route('/see_application', type='http', auth='public', website=True)
-    def app_form(self, **kw):
-
-        students = request.env['se.application'].sudo().search([])
-        return request.render('smartedu_admission.applications_page', {
-            'students': students
+    # Render Form View
+    @http.route('/application', auth='public', website=True)
+    def hospital_patient_new(self, **kw):
+            student = request.env['se.application'].sudo().search([])
+            program = request.env['se.program'].sudo().search([])
+            shift = request.env['se.education.shift'].sudo().search([])
+            return request.render('smartedu_admission.application_form', {
+                'student': student,
+                'program': program,
+                'shift': shift,
         })
-
+    
     # Redirect To  the Page
     @http.route('/student_form', auth='public', website=True)
-    def hospital_patient_redirect(self, **kw):
+    def registration_form_redirect(self, **kw):
         first_name = kw.get('first_name')
         middle_name = kw.get('middle_name')
         last_name = kw.get('last_name')
         application_number= kw.get('application_number')
-        admission_date= kw.get('admission_date')
-        application_date= kw.get('application_date')
+        # admission_date= kw.get('admission_date')
+        # # application_date= kw.get('application_date')
         gender=kw.get('gender')
         mobile=kw.get('mobile')
         academic_medium_type=kw.get('academic_medium_type')
         form_type=kw.get('form_type')
-        birth_date=kw.get('birth_date')
+        # # birth_date=kw.get('birth_date')
         religion=kw.get('religion')
         marital_status=kw.get('marital_status')
         blood_group=kw.get('blood_group')
@@ -41,8 +45,8 @@ class applications(http.Controller):
         father_email=kw.get('father_email')
         father_national_id=kw.get('father_national_id')
         father_passport=kw.get('father_passport')
-        father_birthday=kw.get('father_birthday')
-        father_age=kw.get('father_age')
+        # father_birthday=kw.get('father_birthday')
+        # # father_age=kw.get('father_age')
         father_occupation=kw.get('father_occupation')
         father_company=kw.get('father_company')
         father_designation=kw.get('father_designation')
@@ -52,8 +56,8 @@ class applications(http.Controller):
         mother_email=kw.get('mother_email')
         mother_national_id=kw.get('mother_national_id')
         mother_passport=kw.get('mother_passport')
-        mother_birthday=kw.get('mother_birthday')
-        mother_age=kw.get('mother_age')
+        # # mother_birthday=kw.get('mother_birthday')
+        # # mother_age=kw.get('mother_age')
         mother_occupation=kw.get('mother_occupation')
         mother_company=kw.get('mother_company')
         mother_designation=kw.get('mother_designation')
@@ -68,12 +72,12 @@ class applications(http.Controller):
         life_insurance=kw.get('life_insurance')
         roll_number_ssc=kw.get('roll_number_ssc')
         ssc_grade=kw.get('ssc_grade')
-        year_ssc=kw.get('year_ssc')
+        # year_ssc=kw.get('year_ssc')
         registration_number_ssc=kw.get('registration_number_ssc')
         ssc_gpa=kw.get('ssc_gpa')
         ssc_certificate=kw.get('ssc_certificate')
         is_golden_ssc=kw.get('is_golden_ssc')
-        year_hsc=kw.get('year_hsc')
+        # year_hsc=kw.get('year_hsc')
         roll_number_hsc=kw.get('roll_number_hsc')
         hsc_gpa=kw.get('hsc_gpa')
         is_golden_hsc=kw.get('is_golden_hsc')
@@ -92,22 +96,22 @@ class applications(http.Controller):
         is_first_division_player=kw.get('is_first_division_player')
         passport=kw.get('passport')
         emergency_contact_info=kw.get('emergency_contact_info')
-        passport_expire_date=kw.get('passport_expire_date')
+        # # passport_expire_date=kw.get('passport_expire_date')
         visa_no=kw.get('visa_no')
-        visa_expire_date=kw.get('visa_expire_date')
+        # # visa_expire_date=kw.get('visa_expire_date')
         # program_id=kw.get('program_id')
         applicant_type=kw.get('applicant_type')
         # program_type_id=kw.get('program_type_id')
         last_completed_degree=kw.get('last_completed_degree')
         email = kw.get('email')
         application_number= kw.get('application_number')
-        admission_date= kw.get('admission_date')
-        application_date= kw.get('application_date')
+        # # admission_date= kw.get('admission_date')
+        # # application_date= kw.get('application_date')
         gender=kw.get('gender')
         mobile=kw.get('mobile')
         academic_medium_type=kw.get('academic_medium_type')
         form_type=kw.get('form_type')
-        birth_date=kw.get('birth_date')
+        # # birth_date=kw.get('birth_date')
         religion=kw.get('religion')
         marital_status=kw.get('marital_status')
         blood_group=kw.get('blood_group')
@@ -124,8 +128,8 @@ class applications(http.Controller):
         father_email=kw.get('father_email')
         father_national_id=kw.get('father_national_id')
         father_passport=kw.get('father_passport')
-        father_birthday=kw.get('father_birthday')
-        father_age=kw.get('father_age')
+        # # father_birthday=kw.get('father_birthday')
+        # # father_age=kw.get('father_age')
         father_occupation=kw.get('father_occupation')
         father_company=kw.get('father_company')
         father_designation=kw.get('father_designation')
@@ -135,8 +139,8 @@ class applications(http.Controller):
         mother_email=kw.get('mother_email')
         mother_national_id=kw.get('mother_national_id')
         mother_passport=kw.get('mother_passport')
-        mother_birthday=kw.get('mother_birthday')
-        mother_age=kw.get('mother_age')
+        # # mother_birthday=kw.get('mother_birthday')
+        # # mother_age=kw.get('mother_age')
         mother_occupation=kw.get('mother_occupation')
         mother_company=kw.get('mother_company')
         mother_designation=kw.get('mother_designation')
@@ -151,12 +155,12 @@ class applications(http.Controller):
         life_insurance=kw.get('life_insurance')
         roll_number_ssc=kw.get('roll_number_ssc')
         ssc_grade=kw.get('ssc_grade')
-        year_ssc=kw.get('year_ssc')
+        # # year_ssc=kw.get('year_ssc')
         registration_number_ssc=kw.get('registration_number_ssc')
         ssc_gpa=kw.get('ssc_gpa')
         ssc_certificate=kw.get('ssc_certificate')
         is_golden_ssc=kw.get('is_golden_ssc')
-        year_hsc=kw.get('year_hsc')
+        # # year_hsc=kw.get('year_hsc')
         roll_number_hsc=kw.get('roll_number_hsc')
         hsc_gpa=kw.get('hsc_gpa')
         is_golden_hsc=kw.get('is_golden_hsc')
@@ -175,9 +179,9 @@ class applications(http.Controller):
         is_first_division_player=kw.get('is_first_division_player')
         passport=kw.get('passport')
         emergency_contact_info=kw.get('emergency_contact_info')
-        passport_expire_date=kw.get('passport_expire_date')
+        # # passport_expire_date=kw.get('passport_expire_date')
         visa_no=kw.get('visa_no')
-        visa_expire_date=kw.get('visa_expire_date')
+        # # visa_expire_date=kw.get('visa_expire_date')
         # program_id=kw.get('program_id')
         applicant_type=kw.get('applicant_type')
         # program_type_id=kw.get('program_type_id')
@@ -187,13 +191,13 @@ class applications(http.Controller):
             'middle_name': middle_name,
             'last_name': last_name,
             'application_number':application_number,
-            'admission_date':admission_date,
-            'application_date':application_date,
+            # # 'admission_date':admission_date,
+            # # 'application_date':application_date,
             'gender':gender,
             'mobile':mobile,
             'academic_medium_type':academic_medium_type,
             'form_type':form_type,
-            'birth_date':birth_date,
+            # # 'birth_date':birth_date,
             'religion': religion,
             'marital_status':marital_status,
             'blood_group':blood_group,
@@ -210,8 +214,8 @@ class applications(http.Controller):
             'father_email':father_email,
             'father_national_id':father_national_id,
             'father_passport':father_passport,
-            'father_birthday':father_birthday,
-            'father_age':father_age,
+            # # 'father_birthday':father_birthday,
+            # # 'father_age':father_age,
             'father_occupation':father_occupation,
             'father_company':father_company,
             'father_designation':father_designation,
@@ -221,8 +225,8 @@ class applications(http.Controller):
             'mother_email':mother_email,
             'mother_national_id':mother_national_id,
             'mother_passport':mother_passport,
-            'mother_birthday':mother_birthday,
-            'mother_age':mother_age,
+            # # 'mother_birthday':mother_birthday,
+            # # 'mother_age':mother_age,
             'mother_occupation':mother_occupation,
             'mother_company':mother_company,
             'mother_designation':mother_designation,
@@ -237,12 +241,12 @@ class applications(http.Controller):
             'life_insurance':life_insurance,
             'roll_number_ssc':roll_number_ssc,
             'ssc_grade':ssc_grade,
-            'year_ssc':year_ssc,
+            # # 'year_ssc':year_ssc,
             'registration_number_ssc':registration_number_ssc,
             'ssc_gpa':ssc_gpa,
             'ssc_certificate':ssc_certificate,
             'is_golden_ssc':is_golden_ssc,
-            'year_hsc':year_hsc,
+            # # 'year_hsc':year_hsc,
             'roll_number_hsc':roll_number_hsc,
             'hsc_gpa':hsc_gpa,
             'is_golden_hsc':is_golden_hsc,
@@ -261,22 +265,22 @@ class applications(http.Controller):
             'is_first_division_player':is_first_division_player,
             'passport':passport,
             'emergency_contact_info':emergency_contact_info,
-            'passport_expire_date':passport_expire_date,
+            # # 'passport_expire_date':passport_expire_date,
             'visa_no':visa_no,
-            'visa_expire_date': visa_expire_date,
+            # # 'visa_expire_date': visa_expire_date,
             # 'program_id': program_ids,
             'applicant_type':applicant_type,
             # 'program_type_id':program_type_id,
             'last_completed_degree':last_completed_degree,
             'email': email,
             'application_number':application_number,
-            'admission_date':admission_date,
-            'application_date':application_date,
+            # # 'admission_date':admission_date,
+            # # 'application_date':application_date,
             'gender':gender,
             'mobile':mobile,
             'academic_medium_type':academic_medium_type,
             'form_type':form_type,
-            'birth_date':birth_date,
+            # # 'birth_date':birth_date,
             'religion': religion,
             'marital_status':marital_status,
             'blood_group':blood_group,
@@ -293,8 +297,8 @@ class applications(http.Controller):
             'father_email':father_email,
             'father_national_id':father_national_id,
             'father_passport':father_passport,
-            'father_birthday':father_birthday,
-            'father_age':father_age,
+            # # 'father_birthday':father_birthday,
+            # # 'father_age':father_age,
             'father_occupation':father_occupation,
             'father_company':father_company,
             'father_designation':father_designation,
@@ -304,8 +308,8 @@ class applications(http.Controller):
             'mother_email':mother_email,
             'mother_national_id':mother_national_id,
             'mother_passport':mother_passport,
-            'mother_birthday':mother_birthday,
-            'mother_age':mother_age,
+            # # 'mother_birthday':mother_birthday,
+            # # 'mother_age':mother_age,
             'mother_occupation':mother_occupation,
             'mother_company':mother_company,
             'mother_designation':mother_designation,
@@ -320,12 +324,12 @@ class applications(http.Controller):
             'life_insurance':life_insurance,
             'roll_number_ssc':roll_number_ssc,
             'ssc_grade':ssc_grade,
-            'year_ssc':year_ssc,
+            # # 'year_ssc':year_ssc,
             'registration_number_ssc':registration_number_ssc,
             'ssc_gpa':ssc_gpa,
             'ssc_certificate':ssc_certificate,
             'is_golden_ssc':is_golden_ssc,
-            'year_hsc':year_hsc,
+            # # 'year_hsc':year_hsc,
             'roll_number_hsc':roll_number_hsc,
             'hsc_gpa':hsc_gpa,
             'is_golden_hsc':is_golden_hsc,
@@ -344,25 +348,16 @@ class applications(http.Controller):
             'is_first_division_player':is_first_division_player,
             'passport':passport,
             'emergency_contact_info':emergency_contact_info,
-            'passport_expire_date':passport_expire_date,
+            # # 'passport_expire_date':passport_expire_date,
             'visa_no':visa_no,
-            'visa_expire_date': visa_expire_date,
+            # # 'visa_expire_date': visa_expire_date,
             # 'program_id': program_ids,         
             'applicant_type':applicant_type,
             # 'program_type_id':program_type_id,
             'last_completed_degree':last_completed_degree,
 
         })
-        redirect = '/see_application'
-        return request.redirect(redirect)
-        # Render Form View
-    @http.route('/application', auth='public', website=True)
-    def hospital_patient_new(self, **kw):
-            student = request.env['se.application'].sudo().search([])
-            program = request.env['se.program'].sudo().search([])
-            shift = request.env['se.education.shift'].sudo().search([])
-            return request.render('smartedu_admission.application_form', {
-                'student': student,
-                'program': program,
-                'shift': shift,
-        })
+        # redirect = '/see_application'
+        return request.render('smartedu_admission.applications_page')
+        
+    
--- static/src/js/script.js
+++ static/src/js/script.js
@@ -1,75 +1,146 @@
 // Next
 function nextSection() {
-  console.log("next");
   var currentSection = document.querySelector(
     'section[style*="display: block"]'
   );
+  let program_type = $('#program_type_id').val();
+  let application_numb = $('#application_numb').val();
+  let program = $('#program_id').val();
+
+
+  let firstName = $('#member_first_name').val();
+  let lastName = $('#member_last_name').val();
+  let email = $('#email').val();
+
 
   if (currentSection.getAttribute("id") === "section1") {
-    document.getElementById("section2").style.display = "block";
-    document.querySelector(".color2").style.backgroundColor = "lightblue";
-  }
-  else if (currentSection.getAttribute("id") === "section2") {
-    document.getElementById("section3").style.display = "block";
-    document.querySelector(".color3").style.backgroundColor = "lightblue";
-  } else if (currentSection.getAttribute("id") === "section3") {
+    if (program_type == '') {
+      $('#program_type_error').text('Please fill out this field');
+    }
+
+    if (application_numb == '') {
+      $('#application_numb_error').text('Please fill out this field');
+    }
+
+    if (program == '') {
+      $('#program_error').text('Please fill out this field');
+    }
+
+    if (program_type != '' && program != '' && application_numb != '') {
+
+      $('#program_type_error').text('');
+
+      $('#application_numb_error').text('');
+
+      $('#program_error').text('');
+
+      currentSection.style.display = "none";
+      document.getElementById("section2").style.display = "block";
+      document.querySelector(".color2").style.backgroundColor = "rgba(178, 219, 233, 0.432)";
+    } else {
+
+      if (program_type != '') {
+        $('#program_type_error').text('');
+      }
+      if (application_numb != '') {
+        $('#application_numb_error').text('');
+      }
+      if (program != '') {
+        $('#program_error').text('');
+      }
+
+
+      document.getElementById("section1").style.display = "block";
+    }
+
+
+  } else if (currentSection.getAttribute("id") === "section2") {
+
+    if (firstName == '') {
+      $('#member_first_name_error').text('Please fill out this field');
+    }
+    if (lastName == '') {
+      $('#last_name_error').text('Please fill out this field');
+    }
+    if (email == '') {
+      $('#member_email_error').text('Please fill out this field');
+    }
+
+    if (firstName != '' && lastName != '') {
+      $('#member_first_name_error').text('');
+      $('#last_name_error').text('');
+      $('#member_email_error').text('');
+
+      currentSection.style.display = "none";
+      document.getElementById("section3").style.display = "block";
+      document.querySelector(".color3").style.backgroundColor = "rgba(178, 219, 233, 0.432)";
+
+    } else {
+      if (firstName != '') {
+        $('#member_first_name_error').text('');
+      }
+      if (lastName != '') {
+        $('#last_name_error').text('');
+      }
+      if (email != '') {
+        $('#member_email_error').text('');
+      }
+
+      document.getElementById("section2").style.display = "block";
+      document.getElementById('section2').scrollIntoView({ behavior: 'smooth' });
+    }
+
+
+  }else if (currentSection.getAttribute("id") === "section3") {
+    currentSection.style.display = "none";
     document.getElementById("section4").style.display = "block";
-    document.querySelector(".color4").style.backgroundColor = "lightblue";
-  } else if (currentSection.getAttribute("id") === "section4") {
-    document.getElementById("section4").style.display = "none";
-    document.getElementById("section5").style.display = "block";
-    document.querySelector("button:last-child").style.display = "none";
-    document.querySelector(".color5").style.backgroundColor = "lightblue";
-    // hide "next" button on last section
-
+    document.querySelector(".color4").style.backgroundColor = "rgba(178, 219, 233, 0.432)";
   }
+  else if (currentSection.getAttribute("id") === "section4") {
 
-  currentSection.style.display = "none";
-  document.querySelector("button:first-child").style.display = "block";
-  // show "previous" button when moving forward
+    currentSection.style.display = "none";
+    document.getElementById("section5").style.display = "block";
+    document.querySelector(".color5").style.backgroundColor = "rgba(178, 219, 233, 0.432)";
+    document.querySelector("button:last-child").style.display = "none"; // hide "next" button on last section
+  }
   document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
+  
+
 // Previous
 function prevSection() {
-  console.log("pre");
-
   var currentSection = document.querySelector(
     'section[style*="display: block"]'
   );
 
   if (currentSection.getAttribute("id") === "section2") {
     document.querySelector(".color2").style.backgroundColor =
-      "rgb(194, 225, 177)";
+      "#d5caec57";
     document.getElementById("section1").style.display = "block";
-    document.querySelector("button:first-child").style.display = "none";
-
-    // hide "previous" button on first section
+    document.querySelector("button:first-child").style.display = "block"; // hide "previous" button on first section
   } else if (currentSection.getAttribute("id") === "section3") {
     document.querySelector(".color3").style.backgroundColor =
-      "rgb(194, 225, 177)";
+      "#d5caec57";
     document.getElementById("section2").style.display = "block";
 
   } else if (currentSection.getAttribute("id") === "section4") {
     document.querySelector(".color4").style.backgroundColor =
-      "rgb(194, 225, 177)";
+      "#d5caec57";
     document.getElementById("section3").style.display = "block";
 
   } else if (currentSection.getAttribute("id") === "section5") {
     document.querySelector(".color5").style.backgroundColor =
-      "rgb(194, 225, 177)";
+      "#d5caec57";
     document.getElementById("section4").style.display = "block";
     document.getElementById("section5").style.display = "none";
-    document.querySelector("button:last-child").style.display = "block";
-    // show "next" button when moving back from last section
+    //     document.querySelector("button:last-child").style.display = "block"; // show "next" button when moving back from last section
   }
 
   currentSection.style.display = "none";
-  document.querySelector("button:last-child").style.display = "block";
-  // show "next" button when moving backward
+  document.querySelector("button:last-child").style.display = "block"; // show "next" button when moving backward
   document.body.scrollTop = document.documentElement.scrollTop = 0;
 
 }
-
 // function for checkbox  (permanent address)  (present address)  
 function changeSelect2() {
   console.log("add");
@@ -140,18 +211,3 @@ function changeSelect2() {
 }
 
 
-// function handleProfileUpload(event) {
-//   const file = event.target.files[0];
-
-//   const reader = new FileReader();
-//   reader.readAsDataURL(file);
-//   reader.onload = () => {
-//     const imageDataUrl = reader.result;
-
-//     const image = document.createElement('img');
-//     image.src = imageDataUrl;
-
-//     const imageContainer = document.querySelector('#preview_profile_picture');
-//     imageContainer.appendChild(image);
-//   };
-// }
